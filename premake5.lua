fcaa9e5b557d7987bf045b9b4c21807835014ba3
Anar ABETAYEVA <anar.abetayeva@amadeus.com>
2025-05-23 08:43:37 +0200

    prmake changed

--- premake5.lua
+++ premake5.lua
@@ -14,18 +14,18 @@ project "GLFW"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
 	}
 
 	filter "system:windows"
-        buildoptions {"-std=c11", "-lgdi32"}
 		systemversion "latest"
         staticruntime "On"
 
 		files
 		{
-            "src/platform.c",
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_module.c",
