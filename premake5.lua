20b260314e32eac1009a537e3c650810d240af9d
Anar ABETAYEVA <anar.abetayeva@amadeus.com>
2025-05-26 14:49:59 +0200

    Add premake5.lua to GLFW 3.3.8

---
+++ premake5.lua
@@ -0,0 +1,70 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+		"src/null_platform.h",
+        "src/null_joystick.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+        staticruntime "On"
+
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/win32_platform.c",
+			"src/win32_platform.h"   
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+            "_GLFW_USE_WIN32",
+			"_CRT_SECURE_NO_WARNINGS",
+			"_GLFW_BUILD_DLL"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter { "system:windows", "configurations:Debug-AS" }	
+		runtime "Debug"
+		symbols "on"
+		sanitize { "Address" }
+		flags { "NoRuntimeChecks", "NoIncrementalLink" }
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "speed"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "speed"
+        symbols "off"
